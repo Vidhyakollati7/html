cd38a053797ebce418ab13d39cbb101ff8cf4a28
Vidhyakollati7 <vidhyakollati7@gmail.com>
2025-06-18 14:16:48 +0530

    Add files via upload

---
+++ operartors in java/Ari.java
@@ -0,0 +1,13 @@
+class Ari
+{
+public static void main(String[] args)
+{
+int a=4;
+int b=5;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+}
+}
