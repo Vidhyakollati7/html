8c84e3efb3004472054f67fcc56d315847d588a3
Vidhyakollati7 <vidhyakollati7@gmail.com>
2025-06-19 11:20:36 +0530

    Add files via upload

---
+++ Types of Method Example/Method.java
@@ -0,0 +1,60 @@
+/*without parameter,return
+class Method
+{
+ public void add()
+ {
+   System.out.println(10+2);
+ }
+public static void main(String[] args)
+  {
+   Method s=new Method();
+   s.add();
+  }
+}
+*/
+
+/*with parameter,without return
+class Method {
+  public void sub(int a, int b) {
+    System.out.println(a - b);
+  }
+
+  public static void main(String[] args) {
+    Method s = new Method();
+    s.sub(10, 2);
+  }
+}
+*/
+
+
+/*without parameter,with return
+class Method
+{
+ public void mul()
+ {
+   System.out.println(10*2);
+ }
+public static void main(String[] args)
+  {
+   Method s=new Method();
+   s.mul();
+ return;
+  }
+
+}
+*/
+
+
+/*with parameter,with return
+class Method {
+  public void div(int a, int b) {
+    System.out.println(a / b);
+  }
+
+  public static void main(String[] args) {
+    Method s = new Method();
+    s.div(10, 2);
+return;
+  }
+}
+*/
