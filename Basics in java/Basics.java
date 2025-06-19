47b2a1316a2da8e40a40400f83609248c04afbf6
Vidhyakollati7 <vidhyakollati7@gmail.com>
2025-06-20 01:16:18 +0530

    Add files via upload

---
+++ Basics in java/Basics.java
@@ -0,0 +1,89 @@
+//4.Define variables for data type int,boolean,char,float,double..
+/*
+
+class Basics
+{
+    public static void main(String[] args)
+    {
+        int a=24;
+        boolean b=true;
+        char c='A';
+        float d= 3.14f;   
+        double e= 99.99;
+        
+        System.out.println("Integer value: "+a);
+        System.out.println("Boolean value: "+b);
+        System.out.println("Character value: "+c);
+        System.out.println("Float value: " +d);
+        System.out.println("Double value: " +e);
+    }
+}
+
+Output:
+
+Integer value: 24
+Boolean value: true
+Character value: A
+Float value: 3.14
+Double value: 99.99
+*/
+
+
+// 5. Define local and Global variable with same name and print the both variables..
+
+/*
+
+public class Basics 
+{
+    int x = 10;
+    
+        
+
+    public void local() {
+        int x = 20;
+
+        System.out.println("Local variable : " + x);   
+        
+    }
+    
+
+    public static void main(String[] args) 
+    {
+        Basics b = new Basics();
+        b.local();
+        
+        System.out.println("Global Variable: "+b.x);
+    }
+}
+
+Output:
+Local variable : 20
+Global Variable: 10
+
+*/
+
+//  6.write a function to print your name and call the function from main method..
+
+
+/*
+
+
+
+class Basics
+{
+    public void name()
+    {
+        System.out.println("K.VIDHYA");
+    }
+    public static void main(String[] args) 
+    {
+        Basics b=new Basics();
+        b.name();
+    }
+}
+
+Output:
+
+K.VIDHYA
+
+*/
