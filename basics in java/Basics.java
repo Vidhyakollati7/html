65379c91951bc74ff035dd0a4001363362663e84
Vidhyakollati7 <vidhyakollati7@gmail.com>
2025-06-19 15:09:12 +0530

    Add files via upload

---
+++ basics in java/Basics.java
@@ -0,0 +1,60 @@
+/* creating method,class,object
+
+
+class Basics
+{
+	void sign()
+	{
+	   System.out.println("K.Vidhya");
+	}
+	public static void main(String[] args)
+	{
+	    Basics b=new Basics();
+                    b.sign();
+	}
+}
+
+*/
+
+/*  Print your name
+
+
+public class Basics
+ {
+    public static void main(String[] args)
+          {
+              System.out.println("K.Vidhya");
+           }
+ }
+*/
+
+
+/* single  line comment, Multi-line comment,documentation comment
+class Basics
+{
+	/**void sign()
+	{
+	   System.out.println("K.Vidhya");
+	}*/
+	public static void main(String[] args)
+	{
+	   /* Basics b=new Basics();
+                    b.sign();*/
+	 //System.out.println("Pickachu");
+                    System.out.println("ECE");
+	}
+}
+
+Output:
+ECE
+*/
+class Basics
+{
+	int a=10;
+                 bool
+	public static void main(String[] args)
+	{
+	    Basics b=new Basics();
+                    b.sign();
+	}
+}
