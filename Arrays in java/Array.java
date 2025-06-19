54c6a8761c1fc221f06a1979d90a46e37c858b19
Vidhyakollati7 <vidhyakollati7@gmail.com>
2025-06-19 12:20:54 +0530

    Add files via upload

---
+++ Arrays in java/Array.java
@@ -0,0 +1,37 @@
+
+/*add  integer values in arrays
+
+class Array
+ {
+    public static void main(String[] args)
+	{
+       	  int a[] = {4,6,8,2,};
+       	  int sum = a[0] + a[1] + a[2] + a[3] ;
+       	  System.out.println(sum);
+    	}
+ }
+
+Output:
+20
+*/
+
+/*Calculate average value
+class Array
+ {
+    public static void main(String[] args)
+	{
+       	  int a[] = {24,22,44};
+                   int length =a.length;
+       	  int sum = a[0] + a[1] + a[2];
+	  int c=sum/length;
+       	  System.out.println(c);
+    	}
+ }
+
+Output:
+30
+*/
+
+
+
+
