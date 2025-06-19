d26836476c0c7feb7e3128da066bd212e11d6cf0
Vidhyakollati7 <vidhyakollati7@gmail.com>
2025-06-19 14:22:51 +0530

    Add files via upload

--- patterns in java/Pattern.java
+++ patterns in java/Pattern.java
@@ -1,8 +1,9 @@
-  class  Pattern
+
+ /* 1
+class  Pattern
 {
 public static void main(String args[])
 {
-   /* 1
  int i,j;
 for(i=0; i<5; i++)
 {
@@ -15,7 +16,14 @@ System.out.println(" ");
 }
 }
 */
+
+
 /*2
+
+class  Pattern
+{
+public static void main(String args[])
+{
  int i,j;
 for(i=1; i<6; i++)
 {
@@ -28,7 +36,11 @@ System.out.println( );
 }
 }*/
 
-  /*3
+/*3
+class  Pattern
+{
+public static void main(String args[])
+{
  int i,j;
 for(i=1; i<6; i++)
 {
@@ -40,7 +52,13 @@ System.out.println( );
 }
 }
 }*/
+
+
 /* 4
+  class  Pattern
+{
+public static void main(String args[])
+{
 int i,j;
 int n=1;
 for(i=1;i<6; i++)
@@ -54,7 +72,12 @@ System.out.println();
 }
 }
 }*/
+
 /* 5
+  class  Pattern
+{
+public static void main(String args[])
+{
 int i,j;
 int n=1;
 for(i=1;i <6;i++)
@@ -72,7 +95,12 @@ System.out.println(" ");
 }
 }
 */
+
 /*6
+class  Pattern
+{
+public static void main(String args[])
+{
 int i,j;
 int n=1;
 for(i=1;i<5; i++)
@@ -88,7 +116,12 @@ System.out.println(" ");
 }
 }
 }*/
+
 /* 7
+class  Pattern
+{
+public static void main(String args[])
+{
 int i,j;
 int n=1;
 for(i=1;i <6;i++)
@@ -105,21 +138,12 @@ System.out.println(" ");
 }
 }
 }*/
-/* 19
-int i,j;
 
-for(i=0; i<=6; i++)
-{
-for(j=1; j<=i; j++)
-{
-System.out.print("*");
-}
-System.out.println(i);
-}
-}
-}
-*/ 
 /* 8
+class  Pattern
+{
+public static void main(String args[])
+{
 int i,j;
 
 for(i=0; i<=6; i++)
@@ -132,30 +156,16 @@ System.out.println( );
 }
 }
 }*/
-/* 15
-int n=7;
 
-for( int i=1;i<=n;i++)
-{
-for(int  j=1;j<=n;j++)
-{
-if(i==n/2+1|| j==n/2+4)
- System.out.print("* ");
-else
-System.out.print(" ");
-}
-System.out.println(" ");
-}
-}
-}
-*/    /* 10 
- String word="INDIA";
-for(int i=1; i<=word.length; i++)
-{System.out.println(word.substring(0,i));
-}
-}
-}*/ 
+
+
+
+ 
 /* 8
+class  Pattern
+{
+public static void main(String args[])
+{
  for(int i=1; i<6;i++)
 {
 for( int j=1;j<6; j++)
@@ -172,7 +182,100 @@ System.out.println(" ");
 }
 }
 */ 
+
+/* 9
+class  Pattern
+{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++){
+for(int j=1;j<=5-i;j++){
+System.out.print(" ");
+}
+for(int k=1;k<=i;k++){
+System.out.print((2*k) + " ");
+}
+System.out.println();
+}
+}
+}
+     2
+   2 4
+  2 4 6
+ 2 4 6 8
+2 4 6 8 10
+*/
+
+
+
+/* 10 
+class  Pattern
+{
+public static void main(String args[])
+{
+ String word="INDIA";
+for(int i=1; i<=word.length; i++)
+{System.out.println(word.substring(0,i));
+}
+}
+}*/
+
+
+/* 11
+class  Pattern
+{
+public static void main(String args[])
+{
+int n=5;
+for(int i=1; i<=n;i++)
+{
+for( int j=1;j<n; j++)
+System.out.println( );
+for( int k=1;k<=(2*i-1); k++)
+ System.out.print("*");
+}
+System.out.println( );
+}
+}
+*/
+
+
+
+/* 12
+class  Pattern
+{
+public static void main(String args[])
+{
+int n = 4;
+for (int i = 1; i <= n; i++) {
+for (int j = i; j < n; j++) {
+System.out.print(" ");
+} 
+for (int j = 1; j <= (2 * i - 1); j++) 
+{
+System.out.print("*");
+ }
+System.out.println();
+ }
+ for (int i = n - 1; i >= 1; i--) {
+ for (int j = n; j > i; j--) {
+  System.out.print(" ");
+  }
+  for (int j = 1; j <= (2* i - 1); j++) {
+  System.out.print("*");
+  }
+   System.out.println();
+    }
+    }
+   }
+*/
+
+
 /* 14
+class  Pattern
+{
+public static void main(String args[])
+{
 for(int i=0; i<6; i++)
 {
 for(int j=0; j<6; j++)
@@ -188,13 +291,45 @@ System.out.print(" ");
 System.out.println( );
 }}
 }
+
+
+
  ******
 *          *
 *          *
 *          *
 *          *
  ******
-*//* 17
+
+/* 15
+class  Pattern
+{
+public static void main(String args[])
+{
+int n=7;
+
+for( int i=1;i<=n;i++)
+{
+for(int  j=1;j<=n;j++)
+{
+if(i==n/2+1|| j==n/2+4)
+ System.out.print("* ");
+else
+System.out.print(" ");
+}
+System.out.println(" ");
+}
+}
+}
+*/    
+
+
+
+/* 17
+class  Pattern
+{
+public static void main(String args[])
+{
  for(int i=1; i<=7; i++)
 {
 for(int j=1; j<=7; j++)
@@ -216,57 +351,24 @@ System.out.println( );
 *  *  *
 *******
 */
-/* 11
-int n=5;
-for(int i=1; i<=n;i++)
-{
-for( int j=1;j<n; j++)
-System.out.println( );
-for( int k=1;k<=(2*i-1); k++)
- System.out.print("*");
-}
-System.out.println( );
-}
-}
-*/
-/* 9
-for(int i=1;i<=5;i++){
-for(int j=1;j<=5-i;j++){
-System.out.print(" ");
-}
-for(int k=1;k<=i;k++){
-System.out.print((2*k) + " ");
-}
-System.out.println();
-}
-}
-}
-     2
-   2 4
-  2 4 6
- 2 4 6 8
-2 4 6 8 10
-*/
-/* 12
- int n = 4;
 
-for (int i = 1; i <= n; i++) {
- for (int j = i; j < n; j++) {
-   System.out.print(" ");
-} for (int j = 1; j <= (2 * i - 1); j++) {
+
+
+/* 19
+class  Pattern
+{
+public static void main(String args[])
+{
+int i,j;
+
+for(i=0; i<=6; i++)
+{
+for(j=1; j<=i; j++)
+{
 System.out.print("*");
- }
-System.out.println();
- }
- for (int i = n - 1; i >= 1; i--) {
- for (int j = n; j > i; j--) {
-  System.out.print(" ");
-  }
-  for (int j = 1; j <= (2* i - 1); j++) {
-  System.out.print("*");
-  }
-   System.out.println();
-    }
-    }
-   }
-*/
+}
+System.out.println(i);
+}
+}
+}
+*/ 
